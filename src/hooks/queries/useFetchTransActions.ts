d0ecfce715542d1cf05121e5b144c6b78cda5cc2
Stepan <stepanwdr@gmail.com>
2024-01-18 19:21:46 +0400

    added drawer

--- src/hooks/queries/useFetchTransActions.ts
+++ src/hooks/queries/useFetchTransActions.ts
@@ -15,7 +15,6 @@ const useFetchTransActions = ({userId}: { userId: string }) => {
                 const response = await axios.get(`https://test.gefara.xyz/api/v1/user/${userId}/transactions`);
                 setData(response.data);
                 setIsLoading(false);
-                console.log(response.data,"sdsd")
             } catch (error) {
                 console.error("Error fetching users:", error);
                 setError("An error occurred while fetching users.");
@@ -28,8 +27,6 @@ const useFetchTransActions = ({userId}: { userId: string }) => {
 
 
     }, [userId]);
-
-    console.log(data)
 
     return {
         data,
